371c20ced9e160ac3603ae91b104260d9bda20aa
kgy9425 <kim963^^>
2021-03-02 12:59:10 +0900

    Initial commit

---
+++ src/main/java/com/example/springPractice/controller/FirstController.java
@@ -0,0 +1,16 @@
+package com.example.springPractice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model)
+    {
+        model.addAttribute("username", "Kim");
+        return "greetings"; //templates/greeting.mustache -> 브라우저로 전송
+    }
+}
